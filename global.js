38f871c05327b83d38b516ffefdd68e5f12cb92b
Icebolt365337 <uday.lingampalli@gmail.com>
2025-10-15 22:06:39 -0700

    ok

--- global.js
+++ global.js
@@ -4,6 +4,10 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/website/";         // GitHub Pages repo name
+
 let pages = [
   { url: '', title: 'Home' },
   { url: 'projects/', title: 'Projects' },
@@ -17,5 +21,6 @@ document.body.prepend(nav);
 for (let p of pages) {
   let url = p.url;
   let title = p.title;
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
   nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
 }
