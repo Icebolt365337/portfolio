37789e1c59afece4da6e0ee99f75b37e90ad5dbf
Icebolt365337 <uday.lingampalli@gmail.com>
2025-10-15 22:10:45 -0700

    nav

--- global.js
+++ global.js
@@ -6,7 +6,7 @@ function $$(selector, context = document) {
 
 const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
   ? "/"                  // Local server
-  : "/website/";         // GitHub Pages repo name
+  : "/portfolio/";         // GitHub Pages repo name
 
 let pages = [
   { url: '', title: 'Home' },
@@ -22,5 +22,14 @@ for (let p of pages) {
   let url = p.url;
   let title = p.title;
   url = !url.startsWith('http') ? BASE_PATH + url : url;
-  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+  nav.append(a);
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add('current');
+  }
+  if (a.host !== location.host) {
+    a.target = "_blank";
+  }
 }
