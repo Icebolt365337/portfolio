75035506fd7984f7fa34c6e8f3454a6e248f1006
Icebolt365337 <uday.lingampalli@gmail.com>
2025-10-31 17:46:22 -0700

    update search

--- projects/projects.js
+++ projects/projects.js
@@ -1,48 +1,52 @@
 import { fetchJSON, renderProjects } from '../global.js';
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-let arc = d3.arc().innerRadius(0).outerRadius(50)({
-  startAngle: 0,
-  endAngle: 2 * Math.PI,
-});
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 
-let rolledData = d3.rollups(
-  projects,
-  (v) => v.length,
-  (d) => d.year,
-);
-let data = rolledData.map(([year, count]) => {
-  return { value: count, label: year };
-});
-let sliceGenerator = d3.pie().value((d) => d.value);
-let arcData = sliceGenerator(data);
-let arcs = arcData.map((d) => arcGenerator(d));
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
-arcs.forEach((arc, idx) => {
-    d3.select('svg')
-      .append('path')
-      .attr('d', arc)
-      .attr('fill', colors(idx));
-});
-let legend = d3.select('.legend');
-data.forEach((d, idx) => {
-  legend
+
+function renderPieChart(projectsGiven) {
+  let rolledData = d3.rollups(
+    projectsGiven,
+    (v) => v.length,
+    (d) => d.year,
+  );
+  let data = rolledData.map(([year, count]) => {
+    return { value: count, label: year };
+  });
+  let sliceGenerator = d3.pie().value((d) => d.value);
+  let arcData = sliceGenerator(data);
+  let arcs = arcData.map((d) => arcGenerator(d));
+  let newSVG = d3.select('svg');
+  newSVG.selectAll('path').remove();
+  arcs.forEach((arc, idx) => {
+    newSVG
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', colors(idx));
+  });
+  let newLegend = d3.select('.legend');
+  newLegend.selectAll('li').remove();
+  data.forEach((d, idx) => {
+    newLegend
     .append('li')
     .attr('style', `--color:${colors(idx)}`)
     .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
-});
-renderProjects(projects, projectsContainer, 'h3');
-let query = '';
+  });
+}
 
+renderProjects(projects, projectsContainer, 'h3');
+renderPieChart(projects);
+let query = '';
 let searchInput = document.querySelector('.searchBar');
 
 searchInput.addEventListener('change', (event) => {
-    query = event.target.value;
-    let filteredProjects = projects.filter((project) => {
-        let values = Object.values(project).join('\n').toLowerCase();
-        return values.includes(query.toLowerCase());
-    });
-    renderProjects(filteredProjects, projectsContainer, 'h3');
+  query = event.target.value;
+  let filteredProjects = projects.filter((project) => {
+    let values = Object.values(project).join('\n').toLowerCase();
+    return values.includes(query.toLowerCase());
+  });
+  renderProjects(filteredProjects, projectsContainer, 'h3');
+  renderPieChart(filteredProjects);
 });
